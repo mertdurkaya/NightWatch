8b2ba520dc471933f4dba1265feacd2c747ea687
Mert Durkaya <mertdurkaya@gmail.com>
2023-11-20 23:12:55 +0100

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Mert Durkaya on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
