0265a79c810fecf9970270869a9d4166466e6825
Mert Durkaya <mertdurkaya@gmail.com>
2023-11-22 00:52:10 +0100

    TabView added

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -7,96 +7,19 @@
 
 import SwiftUI
 
-let nightlyTasks = [
-    "Check all windows",
-    "Check all doors",
-    "Check safe",
-    "Check mailbox",
-    "Inspect security cameras",
-    "Clear ice from sidewalks",
-    "Document 'strange and unusual' occurrences"
-]
-
-let weeklyTasks = [
-    "Check inside all rooms",
-    "Walk the premieter of property"
-]
-
-let monthlyTasks = [
-    "Test security alarm",
-    "Test motion detection",
-    "Test smoke alarms"
-]
-
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: HStack(alignment: .bottom) {
-                    Text(Image(systemName: "moon.stars"))
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                    
-                    Text("Nightly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                        .padding(.top)
-                        .textCase(.uppercase)
-                }) {
-                    ForEach(nightlyTasks, id: \.self, content: {
-                        taskName in
-                        NavigationLink(taskName, destination: VStack {
-                            Text(taskName)
-                            Text("Placeholder despription")
-                            Text("Placeholder mark button")
-                        })
-                    })
+        TabView {
+            HomeView()
+                .badge(2)
+                .tabItem {
+                    Label("Home", systemImage: "house")
                 }
-                
-                Section(header: HStack(alignment: .bottom) {
-                    Text(Image(systemName: "sunset"))
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                    
-                    Text("Weekly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                        .padding(.top)
-                        .textCase(.uppercase)
-                }) {
-                    ForEach(weeklyTasks, id: \.self, content: {
-                        taskName in
-                        NavigationLink(taskName, destination: Text(taskName))
-                    })
-                }
-                
-                Section(header: HStack(alignment: .bottom) {
-                    Text(Image(systemName: "calendar"))
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                    
-                    Text("Monthly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.yellow)
-                        .padding(.top)
-                        .textCase(.uppercase)
-                }) {
-                    ForEach(monthlyTasks, id: \.self, content: {
-                        taskName in
-                        NavigationLink(taskName, destination: Text(taskName))
-                    })
-                }
+            DetailsView(taskName: "Test Task")
+                .badge("!")
+                .tabItem {
+                    Label("Test", systemImage: "testtube.2")
             }
-            .navigationTitle("Home")
         }
     }
 }
@@ -104,3 +27,5 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
