ec2ef5e7de5ba84aab9e40da7ea57bdefb085d13
Mert Durkaya <mertdurkaya@gmail.com>
2023-11-21 01:06:32 +0100

    Building SingleView App
    Updated ContentView.swift

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -10,12 +10,84 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    // MARK: Nigthly
+                    // TODO: Do not forget to make them seperate views
+                    // FIXME: It's not broken, only way of showing 'fixme'
+                    Group {
+                        HStack(alignment: .bottom) {
+                            Text(Image(systemName: "moon.stars"))
+                                .symbolRenderingMode(.multicolor)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(.yellow)
+                                .padding(.top)
+                                .textCase(.uppercase)
+                        }
+                        .padding(.bottom)
+                    }
+                    Text("Check all windows ")
+                    Text("Check all doors")
+                    Text("Check safe")
+                    Text("Check mailbox")
+                    Text("Inspect security cameras")
+                    Text("Clear ice from sidewalks")
+                    Text("Document 'strange and unusual' occurrences")
+                    Divider()
+                    
+                    // MARK: Weekly
+                    Group {
+                        HStack(alignment: .bottom) {
+                            Text(Image(systemName: "sunset"))
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(.yellow)
+                                .padding(.top)
+                                .textCase(.uppercase)
+                        }
+                        .padding(.bottom)
+                    }
+                    Text("Check inside all rooms")
+                    Text("Walk the premieter of property")
+                    Divider()
+                    
+                    // MARK: Monthly
+                    Group {
+                        HStack(alignment: .bottom) {
+                            Text(Image(systemName: "calendar"))
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(.yellow)
+                                .padding(.top)
+                                .textCase(.uppercase)
+                        }
+                        .padding(.bottom)
+                        Text("Test security alarm")
+                        Text("Test motion detection")
+                        Text("Test smoke alarms")
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        Spacer()
     }
 }
 
